770db76cde6d78d3aadc3b6b8a1636460ffaefb2
shubhankar9069 <ssingha9069@gmail.com>
2024-02-03 10:51:53 +0530

    next config changed for css modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  cssModules: true
+  cssModules: true,  // Enable CSS modules
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]__[hash:base64:5]",
+  },
 };
 
 export default nextConfig;
