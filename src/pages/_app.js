f24e05573172ae32929f77a544eb9b9ce85e82f9
shubhankar9069 <ssingha9069@gmail.com>
2024-02-03 13:12:31 +0530

    page insights added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import Theme from "@/theme";
 import { ThemeProvider } from "@mui/material";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
-  return <ThemeProvider theme={Theme}><Component {...pageProps} /></ThemeProvider>
+  return <ThemeProvider theme={Theme}><SpeedInsights/><Component {...pageProps} /></ThemeProvider>
 }
