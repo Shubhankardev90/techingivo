b3a608b64e7ed62f4b0b5e35c7947589dfe9cbbb
shubhankar9069 <ssingha9069@gmail.com>
2024-02-03 21:47:14 +0530

    fix

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,23 +3,23 @@ import SectionHeading from './SectionHeading'
 import { Box, Grid } from '@mui/material'
 import { Web } from '@mui/icons-material';
 import FeatureCard from './FeatureCard';
-import {services} from '@/data/services';
+import { services } from '@/data/services';
 const Services = () => {
-  return (
-    <Box py={3} px={3}>
-        <SectionHeading mainHeading="Our Services" description="What we are offering ?" />
-        <Grid container>
-            {
-                services.map(
-                    (service, key)=>(
-                        <Grid item xl={3} lg={4} md={6} sm={6} xs={12}  padding={{lg:'3', md:'3', sm:'1'}} key={key}>
-                            <FeatureCard name={service.name} icon={service.icon}  description={service.description} link={service.link} key={key} />
-                        </Grid>
-                    ))
-            }
-        </Grid>
-    </Box>
-  )
+    return (
+        <Box py={3} px={3}>
+            <SectionHeading mainHeading="Our Services" description="What we are offering ?" />
+            <Grid container spacing={{ xl: 3, lg: 3, md: 2, sm: 1}}>
+                {
+                    services.map(
+                        (service, key) => (
+                            <Grid item xl={3} lg={4} md={6} sm={6} xs={12} key={key}>
+                                <FeatureCard name={service.name} icon={service.icon} description={service.description} link={service.link} key={key} />
+                            </Grid>
+                        ))
+                }
+            </Grid>
+        </Box>
+    )
 }
 
 export default Services
